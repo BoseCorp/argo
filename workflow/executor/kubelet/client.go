06b4e1c089dc6c001779999fa0715869ac7fa11b
Chris Chambers <chris.chambers@peanutcode.com>
2019-03-27 12:27:18 -0400

    Export the methods of `KubernetesClientInterface`
    All calls to these methods previously generated a panic at runtime
    because the calls resolved to the default, panic-always implementation,
    not to the overrides provided by `k8sAPIClient` and `kubeletClient`.
    
    Embedding an exported interface with unexported methods into a struct is
    the only way to implement that interface in another package.  When doing
    this, the compiler generates default, panic-always implementations for
    all methods from the interface.  Implementors can override exported
    methods, but it's not possible to override an unexported method from the
    interface.  All invocations that go through the interface will come to
    the default implementation, even if the struct tries to provide an
    override.

--- workflow/executor/kubelet/client.go
+++ workflow/executor/kubelet/client.go
@@ -30,8 +30,6 @@ const (
 )
 
 type kubeletClient struct {
-	execcommon.KubernetesClientInterface
-
 	httpClient      *http.Client
 	httpHeader      http.Header
 	websocketDialer *websocket.Dialer
@@ -41,6 +39,8 @@ type kubeletClient struct {
 	// - my-host.com:10250
 	kubeletEndpoint string
 }
+
+var _ execcommon.KubernetesClientInterface = &kubeletClient{}
 
 func newKubeletClient() (*kubeletClient, error) {
 	kubeletHost := os.Getenv(common.EnvVarDownwardAPINodeIP)
@@ -179,7 +179,7 @@ func (k *kubeletClient) saveLogsToFile(namespace, podName, containerName, path s
 	return err
 }
 
-func (k *kubeletClient) getContainerStatus(containerID string) (*v1.Pod, *v1.ContainerStatus, error) {
+func (k *kubeletClient) GetContainerStatus(containerID string) (*v1.Pod, *v1.ContainerStatus, error) {
 	podList, err := k.getPodList()
 	if err != nil {
 		return nil, nil, errors.InternalWrapError(err)
@@ -288,7 +288,7 @@ func (k *kubeletClient) readFileContents(u *url.URL) (*bytes.Buffer, error) {
 }
 
 // createArchive exec in the given containerID and create a tarball of the given sourcePath. Works with directory
-func (k *kubeletClient) createArchive(containerID, sourcePath string) (*bytes.Buffer, error) {
+func (k *kubeletClient) CreateArchive(containerID, sourcePath string) (*bytes.Buffer, error) {
 	return k.getCommandOutput(containerID, fmt.Sprintf("command=tar&command=-cf&command=-&command=%s&output=1", sourcePath))
 }
 
@@ -330,7 +330,7 @@ func (k *kubeletClient) WaitForTermination(containerID string, timeout time.Dura
 	return execcommon.WaitForTermination(k, containerID, timeout)
 }
 
-func (k *kubeletClient) killContainer(pod *v1.Pod, container *v1.ContainerStatus, sig syscall.Signal) error {
+func (k *kubeletClient) KillContainer(pod *v1.Pod, container *v1.ContainerStatus, sig syscall.Signal) error {
 	u, err := url.ParseRequestURI(fmt.Sprintf("wss://%s/exec/%s/%s/%s?command=/bin/sh&&command=-c&command=kill+-%d+1&output=1&error=1", k.kubeletEndpoint, pod.Namespace, pod.Name, container.Name, sig))
 	if err != nil {
 		return errors.InternalWrapError(err)
